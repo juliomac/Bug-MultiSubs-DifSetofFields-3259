cfbfcdadd283ee7ad2094bf2dd34f86ac7d77383
juliomac <julio.coutinho@gmail.com>
2015-08-24 11:19:00 -0300

    Fixed silly bug
    Can’t reproduce issue

--- Bug-MultiSubs-DifSetofFields-3259.js
+++ Bug-MultiSubs-DifSetofFields-3259.js
@@ -3,22 +3,50 @@ Cities = new Mongo.Collection('cities');
 
 
 if (Meteor.isClient) {
+  var pulseNode = function(node) {
+      if(!node.style) return;
+      var prev = node.style['background-color'] || 'rgba(255,0,0,0)';
+      $(node).css('background-color', 'red');
+      setTimeout(function() {
+        $(node).css('background-color', prev);
+      }, 1000);
+  };
 
-  Template.BasicData.helpers({
-    MyData: function(){
-      Meteor.subscribe('BasicData');
-      return Cities.find();
-    }
-  });
+  pulse = function(template) {
+      for(var node = template.firstNode; true; node = node.nextSibling) {
+          pulseNode(node);
+          if(node === template.lastNode) return;
+      }
+  }
+
+
+Template.BasicData.onCreated(function(){
+  Meteor.subscribe('BasicData');
+
+});
+
+
+Template.MoreData.onCreated(function(){
+  Meteor.subscribe('MoreData');
+
+});
+
+Template.BasicData.helpers({
+  MyData: function(){
+    return Cities.find();
+  }
+});
+
+
 
   Template.MoreData.helpers({
     MoreOfMydata: function(){
       // Cities._collection.remove({});
       // Cities.stop();
-      Meteor.subscribe('MoreData');
       return Cities.find();
     }
   });
+
 }
 
 if (Meteor.isServer) {
@@ -27,7 +55,11 @@ if (Meteor.isServer) {
     if (Cities.find().count() === 0) {
       Cities.insert(
       {   _id: "BR0310001",
-          City: "Belo Horizonte"
+          City: "Belo Horizonte",
+          details: {
+            detail1: "It is located in Brazil",
+            detail2: "Nice weather all year long"
+          }
       });
     }
 
@@ -36,7 +68,9 @@ if (Meteor.isServer) {
       return Cities.find({_id:"BR0310001"}, {fields: {_id: true}});
     });
 
-    Meteor.publish('MoreData', function() {
-      return Cities.find({_id:"BR0310001"}, {fields: {_id: true, City: true}});
-    });  });
+    Meteor.publish('MoreData', function(check) {
+      return Cities.find({_id:"BR0310001"}, {fields: {_id: true, City: true, 'details.detail1':true}});
+    });
+
+ });
 }
